d5c304dfe78dbfb25cb2a03ead96603bfe8b1e0d
Jovan Nikolov <nikolovjovan@hotmail.com>
2022-07-15 17:57:11 +0200

    WIP spmv gpu - added multiple calls

--- src/gpu/examples/spmv (Parboil)/StrongReproducibility/main.cpp
+++ src/gpu/examples/spmv (Parboil)/StrongReproducibility/main.cpp
@@ -10,6 +10,8 @@
 #include <CL/cl_ext.h>
 #include <cstdio>
 #include <cstdlib>
+#include <cstring>
+#include <chrono>
 #include <random>
 
 #include "parboil.h"
@@ -22,6 +24,40 @@ using namespace std;
 
 constexpr uint32_t DEFAULT_SEED = 1549813198;
 constexpr uint32_t DEFAULT_NUMBER_OF_RUNS = 50;
+
+struct pb_TimerSet timers;
+
+int len;
+int depth;
+int dim;
+int pad = 32;
+int nzcnt_len;
+
+const char *clSource[1];
+
+// OpenCL specific
+cl_int clStatus;
+cl_platform_id clPlatform;
+cl_device_id clDevice;
+OpenCLDeviceProp clDeviceProp;
+cl_context clContext;
+cl_command_queue clCommandQueue;
+cl_program clProgram;
+cl_kernel clKernel;
+
+// device memory allocation
+// matrix
+cl_mem d_data;
+cl_mem d_indices;
+cl_mem d_ptr;
+cl_mem d_perm;
+
+// vector
+cl_mem d_Ax_vector;
+cl_mem d_x_vector;
+
+cl_mem jds_ptr_int;
+cl_mem sh_zcnt_int;
 
 bool generate_vector(float *x_vector, int dim, uint32_t seed)
 {
@@ -49,47 +85,28 @@ bool diff(int dim, float *h_Ax_vector_1, float *h_Ax_vector_2)
     return false;
 }
 
-int main(int argc, char **argv)
+void initOCL()
 {
-    struct pb_TimerSet timers;
-    struct pb_Parameters *parameters;
-
-    printf("OpenCL accelerated sparse matrix vector multiplication****\n");
-    printf("Original version by Li-Wen Chang <lchang20@illinois.edu> and Shengzhao Wu<wu14@illinois.edu>\n");
-    printf("This version maintained by Chris Rodrigues  ***********\n");
-
-    parameters = pb_ReadParameters(&argc, argv);
-    if (parameters->inpFiles[0] == NULL)
-    {
-        fprintf(stderr, "Expecting one input filename\n");
-        exit(-1);
-    }
-
-    pb_InitializeTimerSet(&timers);
-    pb_SwitchToTimer(&timers, pb_TimerID_COMPUTE);
-
-    // parameters declaration
-    cl_int clStatus;
-    cl_platform_id clPlatform;
     clStatus = clGetPlatformIDs(1, &clPlatform, NULL);
     CHECK_ERROR("clGetPlatformIDs")
 
-    cl_context_properties clCps[3] = {CL_CONTEXT_PLATFORM, (cl_context_properties)clPlatform, 0};
-
-    cl_device_id clDevice;
     clStatus = clGetDeviceIDs(clPlatform, CL_DEVICE_TYPE_GPU, 1, &clDevice, NULL);
     CHECK_ERROR("clGetDeviceIDs")
 
-    cl_context clContext = clCreateContextFromType(clCps, CL_DEVICE_TYPE_GPU, NULL, NULL, &clStatus);
+    clStatus = clGetDeviceInfo(clDevice, CL_DEVICE_MAX_COMPUTE_UNITS, sizeof(cl_uint), &(clDeviceProp.multiProcessorCount), NULL);
+    CHECK_ERROR("clGetDeviceInfo")
+
+    cl_context_properties clCps[3] = {CL_CONTEXT_PLATFORM, (cl_context_properties)clPlatform, 0};
+    clContext = clCreateContextFromType(clCps, CL_DEVICE_TYPE_GPU, NULL, NULL, &clStatus);
     CHECK_ERROR("clCreateContextFromType")
 
-    cl_command_queue clCommandQueue = clCreateCommandQueue(clContext, clDevice, CL_QUEUE_PROFILING_ENABLE, &clStatus);
+    clCommandQueue = clCreateCommandQueue(clContext, clDevice, CL_QUEUE_PROFILING_ENABLE, &clStatus);
     CHECK_ERROR("clCreateCommandQueue")
 
     pb_SetOpenCL(&clContext, &clCommandQueue);
 
-    const char *clSource[] = {readFile("./kernel.cl")};
-    cl_program clProgram = clCreateProgramWithSource(clContext, 1, clSource, NULL, &clStatus);
+    clSource[0] = readFile("./kernel.cl");
+    clProgram = clCreateProgramWithSource(clContext, 1, clSource, NULL, &clStatus);
     CHECK_ERROR("clCreateProgramWithSource")
 
     char clOptions[50];
@@ -97,80 +114,9 @@ int main(int argc, char **argv)
     clStatus = clBuildProgram(clProgram, 1, &clDevice, clOptions, NULL, NULL);
     CHECK_ERROR("clBuildProgram")
 
-    cl_kernel clKernel = clCreateKernel(clProgram, "spmv_jds_naive", &clStatus);
+    clKernel = clCreateKernel(clProgram, "spmv_jds_naive", &clStatus);
     CHECK_ERROR("clCreateKernel")
 
-    int len;
-    int depth;
-    int dim;
-    int pad = 32;
-    int nzcnt_len;
-
-    // host memory allocation
-    // matrix
-    float *h_data;
-    int *h_indices;
-    int *h_ptr;
-    int *h_perm;
-    int *h_nzcnt;
-    // vector
-    float *h_Ax_vector;
-    float *h_x_vector;
-
-    // device memory allocation
-    // matrix
-    cl_mem d_data;
-    cl_mem d_indices;
-    cl_mem d_ptr;
-    cl_mem d_perm;
-    cl_mem d_nzcnt;
-
-    // vector
-    cl_mem d_Ax_vector;
-    cl_mem d_x_vector;
-
-    cl_mem jds_ptr_int;
-    cl_mem sh_zcnt_int;
-
-    // load matrix from files
-    pb_SwitchToTimer(&timers, pb_TimerID_IO);
-    // inputData(parameters->inpFiles[0], &len, &depth, &dim,&nzcnt_len,&pad,
-    //     &h_data, &h_indices, &h_ptr,
-    //     &h_perm, &h_nzcnt);
-    int col_count;
-    coo_to_jds(
-        parameters->inpFiles[0], // bcsstk32.mtx, fidapm05.mtx, jgl009.mtx
-        1,                       // row padding
-        pad,                     // warp size
-        1,                       // pack size
-        1,                       // debug level [0:2]
-        &h_data, &h_ptr, &h_nzcnt, &h_indices, &h_perm,
-        &col_count, &dim, &len, &nzcnt_len, &depth);
-
-    //	pb_SwitchToTimer(&timers, pb_TimerID_COMPUTE);
-
-    h_Ax_vector = new float[dim];
-    h_x_vector = new float[dim];
-
-    uint32_t seed = parameters->seed == 0 ? DEFAULT_SEED : parameters->seed;
-    uint32_t nruns = parameters->nruns == 0 ? DEFAULT_NUMBER_OF_RUNS : parameters->nruns;
-
-    if (!generate_vector(h_x_vector, dim, seed))
-    {
-        fprintf(stderr, "Failed to generate dense vector.\n");
-        exit(-1);
-    }
-
-    pb_SwitchToTimer(&timers, pb_TimerID_COMPUTE);
-    OpenCLDeviceProp clDeviceProp;
-    //	clStatus = clGetDeviceInfo(clDevice,CL_DEVICE_COMPUTE_CAPABILITY_MAJOR_NV,sizeof(cl_uint),&(clDeviceProp.major),NULL);
-    // CHECK_ERROR("clGetDeviceInfo")
-    //	clStatus = clGetDeviceInfo(clDevice,CL_DEVICE_COMPUTE_CAPABILITY_MINOR_NV,sizeof(cl_uint),&(clDeviceProp.minor),NULL);
-    //      CHECK_ERROR("clGetDeviceInfo")
-    clStatus = clGetDeviceInfo(clDevice, CL_DEVICE_MAX_COMPUTE_UNITS, sizeof(cl_uint), &(clDeviceProp.multiProcessorCount), NULL);
-    CHECK_ERROR("clGetDeviceInfo")
-
-    pb_SwitchToTimer(&timers, pb_TimerID_COPY);
     // memory allocation
     d_data = clCreateBuffer(clContext, CL_MEM_READ_ONLY, len * sizeof(float), NULL, &clStatus);
     CHECK_ERROR("clCreateBuffer")
@@ -187,7 +133,35 @@ int main(int argc, char **argv)
     CHECK_ERROR("clCreateBuffer")
     sh_zcnt_int = clCreateBuffer(clContext, CL_MEM_READ_ONLY, 5000 * sizeof(int), NULL, &clStatus);
     CHECK_ERROR("clCreateBuffer")
+}
 
+void cleanupOCL()
+{
+    clStatus = clReleaseKernel(clKernel);
+    clStatus |= clReleaseProgram(clProgram);
+
+    clStatus |= clReleaseMemObject(d_data);
+    clStatus |= clReleaseMemObject(d_indices);
+    clStatus |= clReleaseMemObject(d_perm);
+    clStatus |= clReleaseMemObject(d_x_vector);
+    clStatus |= clReleaseMemObject(d_Ax_vector);
+    CHECK_ERROR("clReleaseMemObject")
+
+    clStatus |= clReleaseCommandQueue(clCommandQueue);
+    clStatus |= clReleaseContext(clContext);
+
+    free((void *)clSource[0]);
+
+    CHECK_ERROR("Failed to release OpenCL resources!\n")
+}
+
+void spmv(bool reproducible, const int fpe, const bool early_exit,
+          int dim, int *h_nzcnt, int *h_ptr, int *h_indices, float *h_data,
+          float *h_x_vector, int *h_perm, float *h_Ax_vector)
+{
+    pb_SwitchToTimer(&timers, pb_TimerID_COPY);
+
+    // cleanup memory
     clMemSet(clCommandQueue, d_Ax_vector, 0, dim * sizeof(float));
 
     // memory copy
@@ -235,12 +209,8 @@ int main(int argc, char **argv)
     // main execution
     pb_SwitchToTimer(&timers, pb_TimerID_KERNEL);
 
-    int i;
-    for (i = 0; i < 50; i++)
-    {
-        clStatus = clEnqueueNDRangeKernel(clCommandQueue, clKernel, 1, NULL, &grid, &block, 0, NULL, NULL);
-        CHECK_ERROR("clEnqueueNDRangeKernel")
-    }
+    clStatus = clEnqueueNDRangeKernel(clCommandQueue, clKernel, 1, NULL, &grid, &block, 0, NULL, NULL);
+    CHECK_ERROR("clEnqueueNDRangeKernel")
 
     clStatus = clFinish(clCommandQueue);
     CHECK_ERROR("clFinish")
@@ -250,36 +220,186 @@ int main(int argc, char **argv)
     clStatus = clEnqueueReadBuffer(clCommandQueue, d_Ax_vector, CL_TRUE, 0, dim * sizeof(float), h_Ax_vector, 0, NULL, NULL);
     CHECK_ERROR("clEnqueueReadBuffer")
 
-    clStatus = clReleaseKernel(clKernel);
-    clStatus = clReleaseProgram(clProgram);
+    pb_SwitchToTimer(&timers, pb_TimerID_NONE);
+}
 
-    clStatus = clReleaseMemObject(d_data);
-    clStatus = clReleaseMemObject(d_indices);
-    clStatus = clReleaseMemObject(d_perm);
-    clStatus = clReleaseMemObject(d_nzcnt);
-    clStatus = clReleaseMemObject(d_x_vector);
-    clStatus = clReleaseMemObject(d_Ax_vector);
-    CHECK_ERROR("clReleaseMemObject")
+void execute(uint32_t nruns, bool reproducible, const int fpe, const bool early_exit,
+             int dim, int *h_nzcnt, int *h_ptr, int *h_indices, float *h_data,
+             float *h_x_vector, int *h_perm, float *h_Ax_vector, uint64_t &time)
+{
+    printf("Running %sreproducible implementation...\n", reproducible ? "" : "non-");
 
-    clStatus = clReleaseCommandQueue(clCommandQueue);
-    clStatus = clReleaseContext(clContext);
+    chrono::steady_clock::time_point start;
 
-    if (parameters->outFile)
+    float *tmp_h_Ax_vector = new float[dim];
+
+    for (int i = 0; i < nruns; ++i)
     {
-        pb_SwitchToTimer(&timers, pb_TimerID_IO);
-        outputData(parameters->outFile, h_Ax_vector, dim);
+        if (i == 0)
+        {
+            start = chrono::steady_clock::now();
+        }
+
+        spmv(reproducible, fpe, early_exit, dim, h_nzcnt, h_ptr, h_indices, h_data, h_x_vector, h_perm, tmp_h_Ax_vector);
+
+        if (i == 0)
+        {
+            time = chrono::duration_cast<chrono::microseconds>(chrono::steady_clock::now() - start).count();
+            memcpy(h_Ax_vector, tmp_h_Ax_vector, dim * sizeof(float));
+        }
+        else if (diff(dim, h_Ax_vector, tmp_h_Ax_vector))
+        {
+            printf("%sreproducible implementation not reproducible after %d runs!\n",
+                   reproducible ? "" : "Non-", i);
+            break;
+        }
+    }
+
+    delete[] tmp_h_Ax_vector;
+}
+
+int main(int argc, char **argv)
+{
+    struct pb_Parameters *parameters;
+
+    printf("OpenCL accelerated sparse matrix vector multiplication****\n");
+    printf("Original version by Li-Wen Chang <lchang20@illinois.edu> and Shengzhao Wu<wu14@illinois.edu>\n");
+    printf("This version maintained by Chris Rodrigues  ***********\n");
+
+    parameters = pb_ReadParameters(&argc, argv);
+    if (parameters->inpFiles[0] == NULL)
+    {
+        fprintf(stderr, "Expecting one input filename\n");
+        exit(-1);
+    }
+
+    pb_InitializeTimerSet(&timers);
+    pb_SwitchToTimer(&timers, pb_TimerID_COMPUTE);
+
+    // // parameters declaration
+    // cl_int clStatus;
+    // cl_platform_id clPlatform;
+    // clStatus = clGetPlatformIDs(1, &clPlatform, NULL);
+    // CHECK_ERROR("clGetPlatformIDs")
+
+    // cl_context_properties clCps[3] = {CL_CONTEXT_PLATFORM, (cl_context_properties)clPlatform, 0};
+
+    // cl_device_id clDevice;
+    // clStatus = clGetDeviceIDs(clPlatform, CL_DEVICE_TYPE_GPU, 1, &clDevice, NULL);
+    // CHECK_ERROR("clGetDeviceIDs")
+
+    // cl_context clContext = clCreateContextFromType(clCps, CL_DEVICE_TYPE_GPU, NULL, NULL, &clStatus);
+    // CHECK_ERROR("clCreateContextFromType")
+
+    // cl_command_queue clCommandQueue = clCreateCommandQueue(clContext, clDevice, CL_QUEUE_PROFILING_ENABLE, &clStatus);
+    // CHECK_ERROR("clCreateCommandQueue")
+
+    // pb_SetOpenCL(&clContext, &clCommandQueue);
+
+    // const char *clSource[] = {readFile("./kernel.cl")};
+    // cl_program clProgram = clCreateProgramWithSource(clContext, 1, clSource, NULL, &clStatus);
+    // CHECK_ERROR("clCreateProgramWithSource")
+
+    // char clOptions[50];
+    // sprintf(clOptions, "");
+    // clStatus = clBuildProgram(clProgram, 1, &clDevice, clOptions, NULL, NULL);
+    // CHECK_ERROR("clBuildProgram")
+
+    // cl_kernel clKernel = clCreateKernel(clProgram, "spmv_jds_naive", &clStatus);
+    // CHECK_ERROR("clCreateKernel")
+
+    // host memory allocation
+    // matrix
+    float *h_data;
+    int *h_indices;
+    int *h_ptr;
+    int *h_perm;
+    int *h_nzcnt;
+    // vector
+    float *h_Ax_vector, *h_Ax_vector_acc, *h_Ax_vector_fp2, *h_Ax_vector_fp4, *h_Ax_vector_fp8ee;
+    float *h_x_vector;
+
+    // load matrix from files
+    pb_SwitchToTimer(&timers, pb_TimerID_IO);
+
+    int col_count;
+    coo_to_jds(
+        parameters->inpFiles[0], // bcsstk32.mtx, fidapm05.mtx, jgl009.mtx
+        1,                       // row padding
+        pad,                     // warp size
+        1,                       // pack size
+        1,                       // debug level [0:2]
+        &h_data, &h_ptr, &h_nzcnt, &h_indices, &h_perm,
+        &col_count, &dim, &len, &nzcnt_len, &depth);
+
+    //	pb_SwitchToTimer(&timers, pb_TimerID_COMPUTE);
+
+    h_Ax_vector = new float[dim];
+    h_Ax_vector_acc = new float[dim];
+    h_Ax_vector_fp2 = new float[dim];
+    h_Ax_vector_fp4 = new float[dim];
+    h_Ax_vector_fp8ee = new float[dim];
+
+    h_x_vector = new float[dim];
+
+    uint32_t seed = parameters->seed == 0 ? DEFAULT_SEED : parameters->seed;
+    uint32_t nruns = parameters->nruns == 0 ? DEFAULT_NUMBER_OF_RUNS : parameters->nruns;
+
+    if (!generate_vector(h_x_vector, dim, seed))
+    {
+        fprintf(stderr, "Failed to generate dense vector.\n");
+        exit(-1);
     }
 
     pb_SwitchToTimer(&timers, pb_TimerID_COMPUTE);
 
-    free((void *)clSource[0]);
+    uint64_t time_non_rep, time_acc, time_fp2, time_fp4, time_fp8ee;
+
+    initOCL();
+
+    execute(nruns, false, 0, false, dim, h_nzcnt, h_ptr, h_indices, h_data, h_x_vector, h_perm, h_Ax_vector, time_non_rep);
+    execute(nruns, true, 0, false, dim, h_nzcnt, h_ptr, h_indices, h_data, h_x_vector, h_perm, h_Ax_vector_acc, time_acc);
+    execute(nruns, true, 2, false, dim, h_nzcnt, h_ptr, h_indices, h_data, h_x_vector, h_perm, h_Ax_vector_fp2, time_fp2);
+    execute(nruns, true, 4, false, dim, h_nzcnt, h_ptr, h_indices, h_data, h_x_vector, h_perm, h_Ax_vector_fp4, time_fp4);
+    execute(nruns, true, 8, true, dim, h_nzcnt, h_ptr, h_indices, h_data, h_x_vector, h_perm, h_Ax_vector_fp8ee, time_fp8ee);
+
+    cleanupOCL();
+
+    if (diff(dim, h_Ax_vector, h_Ax_vector_acc))
+    {
+        printf("Non-reproducible and reproducible (accumulator-only) results do not match!\n");
+    }
+
+    if (diff(dim, h_Ax_vector_acc, h_Ax_vector_fp2))
+    {
+        printf("Reproducible acc and fp2 results do not match!\n");
+    }
+
+    if (diff(dim, h_Ax_vector_acc, h_Ax_vector_fp4))
+    {
+        printf("Reproducible acc and fp4 results do not match!\n");
+    }
+
+    if (diff(dim, h_Ax_vector_acc, h_Ax_vector_fp8ee))
+    {
+        printf("Reproducible acc and fp8ee results do not match!\n");
+    }
+
+    printf("Non-reproducible implementation time: %d [us] (%.3f [ms])\n", time_non_rep, (float) time_non_rep / 1000.0);
+    printf("Reproducible acc implementation time: %d [us] (%.3f [ms])\n", time_acc, (float) time_acc / 1000.0);
+    printf("Reproducible fp2 implementation time: %d [us] (%.3f [ms])\n", time_fp2, (float) time_fp2 / 1000.0);
+    printf("Reproducible fp4 implementation time: %d [us] (%.3f [ms])\n", time_fp4, (float) time_fp4 / 1000.0);
+    printf("Reproducible fp8ee implementation time: %d [us] (%.3f [ms])\n", time_fp8ee, (float) time_fp8ee / 1000.0);
 
     delete[] h_data;
     delete[] h_indices;
     delete[] h_ptr;
     delete[] h_perm;
     delete[] h_nzcnt;
-    delete[] h_Ax_vector;
+    delete[] h_Ax_vector_acc;
+    delete[] h_Ax_vector_fp2;
+    delete[] h_Ax_vector_fp4;
+    delete[] h_Ax_vector_fp8ee;
     delete[] h_x_vector;
 
     pb_SwitchToTimer(&timers, pb_TimerID_NONE);
